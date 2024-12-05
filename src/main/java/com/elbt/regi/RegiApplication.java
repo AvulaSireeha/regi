a6a155828526eb43197200aee5d70ffd75c6b0d7
Avula-S <savula@neoterictechnologiesinc.com>
2024-12-05 13:08:58 +0530

    "first commit"

---
+++ src/main/java/com/elbt/regi/RegiApplication.java
@@ -0,0 +1,13 @@
+package com.elbt.regi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/elbt/regi/Registration.java
@@ -0,0 +1,7 @@
+package com.elbt.regi;
+
+public class Registration {
+    private String name;
+    private int age;
+
+}
